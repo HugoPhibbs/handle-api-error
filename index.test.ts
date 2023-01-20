b2d38e618d6252e0db2e24210b89a16e1e3e8715
hugo <hugo@enrolmy.com>
2023-01-20 14:56:36 +1300

    adding expose to function

--- index.test.ts
+++ index.test.ts
@@ -3,13 +3,13 @@ const handleError = require("./index.ts");
 describe("Test creating errors", () => {
     test("Test with normal errors", () => {
         const error = new Error("a random error");
-        const newError = handleError(error, "Running another test");
+        const newError = handleError(error, "Running another test", 400, true);
         expect(newError.status).toBe(400);
     });
 
     test("Test different status codes are recognized", () => {
         const error = new Error("Another random error");
-        const newError = handleError(error, "Running another test", 500);
+        const newError = handleError(error, "Running another test", 500,true );
         expect(newError.status).toBe(500);
         expect(newError.msg).toBe("Error occurred while Running another test");
     });
--- index.ts
+++ index.ts
@@ -8,15 +8,17 @@ const createError = require("http-errors");
  * @param error an error that was thrown
  * @param event string for what was trying to be done when this error occured
  * @param status number for any status code to attach to a new HttpError
+ * @param expose whether error message should be revealed to the client
  * @return HttpError the inputted error parsed into a HttpError
  */
 function handleError(
     error: any,
     event: string,
-    status: number = 400
+    status: number = 400,
+    expose: boolean = false
 ): HttpError {
     let _msg = `Error occurred while ${event}`;
-    return createError(status, { msg: _msg, originalError: error });
+    return createError(status, {originalError:error, msg: _msg, expose : expose});
 }
 
 module.exports = handleError
